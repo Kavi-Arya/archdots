8a0e70efa6d495f3aa1bfde8a9ef9708791ee256
Kavi-Arya <kaviarya047@tutamail.com>
2023-12-11 00:22:40 +0530

    last for a while

--- install.sh
+++ install.sh
@@ -1,7 +1,16 @@
 #!/bin/sh
 
-gitClones() {
+makingDirs() {
+	mkdir $HOME/.clones
+	mkdir $HOME/.config
+	mkdir -p $HOME/.local/bin/
+	mkdir $HOME/Downloads
+	mkdir -p $HOME/Documents/pdfs/
+}
 
+gitClones() {
+	sudo pacman -S git
+	git clone --dept 1 https://github.com/Kavi-Arya/archdots.git
 }
 
 config() {
@@ -9,7 +18,9 @@ config() {
 }
 
 paruInstall() {
-
+	git clone https://aur.archlinux.org/paru.git $HOME/.clones/paru
+	cd $HOME/.clones/paru
+	makepkg -si
 }
 
 dwm() {
@@ -17,5 +28,7 @@ dwm() {
 }
 
 pkgs() {
-
+	sudo pacman -S --needed base-devel
+	sudo pacman -S --needed - <./assets/pkgList.txt
+	paru -S - <./assets/aurPkgs.txt
 }
