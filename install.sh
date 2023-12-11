3c4fabf875acdfb5537a71f928d937b04e3f0478
Kavi-Arya <kaviarya047@tutamail.com>
2023-12-11 10:38:24 +0530

    last for a while

--- install.sh
+++ install.sh
@@ -1,34 +1,69 @@
 #!/bin/sh
+echo "PlaceHolder"
+CONFIGDIR=$("$HOME/.config/")
+ARCHDOTS=$("$HOME/.clones/archdots")
+LOCBIN=$("$HOME/.local/bin")
+AURHELPER="paru"
 
 makingDirs() {
-	mkdir $HOME/.clones
-	mkdir $HOME/.config
-	mkdir -p $HOME/.local/bin/
+	echo "PlaceHolder"
+	mkdir $CONFIGDIR
+	mkdir $ARCHDOTS
+	mkdir -p $LOCBIN
 	mkdir $HOME/Downloads
 	mkdir -p $HOME/Documents/pdfs/
-}
-
-gitClones() {
-	sudo pacman -S git
-	git clone --dept 1 https://github.com/Kavi-Arya/archdots.git
-}
-
-config() {
-
+	echo "PlaceHolder"
 }
 
 paruInstall() {
+	echo "PlaceHolder"
 	git clone https://aur.archlinux.org/paru.git $HOME/.clones/paru
 	cd $HOME/.clones/paru
 	makepkg -si
-}
-
-dwm() {
-
+	echo "PlaceHolder"
 }
 
 pkgs() {
+	echo "PlaceHolder"
+	cd $ARCHDOTS
 	sudo pacman -S --needed base-devel
 	sudo pacman -S --needed - <./assets/pkgList.txt
-	paru -S - <./assets/aurPkgs.txt
+	$AURHELPER -S - <./assets/aurPkgs.txt
+	echo "PlaceHolder"
+}
+
+config() {
+	echo "PlaceHolder"
+	cd $ARCHDOTS
+	cp - rdwm dmenu st kitty zsh $CONFIGDIR
+	cp -r .zprofile $HOME
+	sudo cp -r ./assets/xorgConfig/* /etc/X11/xorg.conf.d/
+	sudo cp -r ./fonts/* /usr/share/fonts/
+	sudo fc-cache -f -v && fc-cache -f -v
+	echo "PlaceHolder"
+}
+
+dwmBuild() {
+	echo "PlaceHolder"
+	cd $CONFIGDIR/dwm
+	rm -rf ./config.h
+	make
+	sudo make install
+	echo "PlaceHolder"
+}
+
+stBuild() {
+	echo "PlaceHolder"
+	cd $CONFIGDIR/st
+	make
+	sudo make install
+	echo "PlaceHolder"
+}
+
+dmenuBuild() {
+	echo "PlaceHolder"
+	cd $CONFIGDIR/dmenu
+	make
+	sudo make install
+	echo "PlaceHolder"
 }
